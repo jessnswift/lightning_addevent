3bf04f43f39a27e46fd84f42a025d786dd5a240a
Jessica Swift <jessica.nicole.swift@gmail.com>
2018-05-23 13:08:57 -0500

    first commit

---
+++ README.md
@@ -0,0 +1,5 @@
+## Lightning Exercise
+
+1. Create three `<h3>` components in your index.html file and add any text you want to each.
+1. Use JavaScript to attach a click event listener to each one.
+1. When the user clicks on any of the components output the content of that one to the developer console.
---
+++ h3.js
@@ -0,0 +1,13 @@
+const one = document.querySelector('#one');
+const two = document.querySelector('#two');
+const three = document.querySelector('#three');
+
+one.addEventListener('click', function() {
+    console.log('One was clicked');
+});
+two.addEventListener('click', function() {
+    console.log('Two was clicked');
+});
+three.addEventListener('click', function() {
+    console.log('Three was clicked');
+})
